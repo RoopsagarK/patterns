b24700920eb083302e758adecf579a5df090989f
Roopsagar K <107497296+RoopsagarK@users.noreply.github.com>
2022-10-11 21:48:51 -0700

    initial co

--- BinaryTriangle.java
+++ BinaryTriangle.java
@@ -1,16 +1,18 @@
 import java.util.*;
-public class FloydsTriangle {
+public class BinaryTriangle {
     public static void main(String args[]){
         int n;
-        int number = 1;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the required row size : ");
         n = sc.nextInt();
- 
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-              System.out.print(number+" ");
-              number++;
+                if( i%2 == 0 && j%2 == 0 || i%2 != 0 && j%2 != 0   ){
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
             }
             System.out.print("\n");
         }
@@ -19,9 +21,9 @@ public class FloydsTriangle {
 /*
  Output:
  Enter the required row size : 5
-1 
-2 3
-4 5 6 
-7 8 9 10
-11 12 13 14 15
+1
+01
+101
+0101
+10101
  */
